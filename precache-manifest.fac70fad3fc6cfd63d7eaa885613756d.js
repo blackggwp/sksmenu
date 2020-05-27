831ec652fbab702938c18a22920f9910e085411d
blackMBP <you@example.com>
2020-05-27 12:00:02 +0700

    Updates

---
+++ precache-manifest.fac70fad3fc6cfd63d7eaa885613756d.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "b41b4c1e603983a5c61f",
+    "url": "/expo-gh-pages/static/js/app.af1d5b17.chunk.js"
+  },
+  {
+    "revision": "a6239f3c6e7a6aff0e06",
+    "url": "/expo-gh-pages/static/js/runtime~app.a96485a6.js"
+  },
+  {
+    "revision": "a09ec40b3ef1cfea068a",
+    "url": "/expo-gh-pages/static/js/2.8da33f4b.chunk.js"
+  },
+  {
+    "revision": "004c2bbb035d8d06bb830efc4673c886",
+    "url": "/expo-gh-pages/static/media/star.004c2bbb.png"
+  },
+  {
+    "revision": "2327736b3ea09c41abfb69be1221f430",
+    "url": "/expo-gh-pages/static/media/heart.2327736b.png"
+  },
+  {
+    "revision": "51671417ef20e0bbc32f0a2bc6edfa95",
+    "url": "/expo-gh-pages/static/media/rocket.51671417.png"
+  },
+  {
+    "revision": "a7b9dc9de5f8f1fb1afbef917619a5ac",
+    "url": "/expo-gh-pages/static/media/bell.a7b9dc9d.png"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/expo-gh-pages/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "3c851d60ad5ef3f2fe43ebd263490d78",
+    "url": "/expo-gh-pages/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/expo-gh-pages/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/expo-gh-pages/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/expo-gh-pages/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/expo-gh-pages/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/expo-gh-pages/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "ca9ce9ff0676a9b04ef0f8a3ad17dd08",
+    "url": "/expo-gh-pages/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/expo-gh-pages/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "b70cea0339374107969eb53e5b1f603f",
+    "url": "/expo-gh-pages/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "c39278f7abfc798a241551194f55e29f",
+    "url": "/expo-gh-pages/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/expo-gh-pages/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/expo-gh-pages/expo-service-worker.js"
+  },
+  {
+    "revision": "afd5733ac789ab1795f1ed7a26e1ecd3",
+    "url": "/expo-gh-pages/favicon-16.png"
+  },
+  {
+    "revision": "3d85359b37ca7590f8e91d593d02ddaf",
+    "url": "/expo-gh-pages/favicon-32.png"
+  },
+  {
+    "revision": "5e52768b0f7cbb62cbe07751add33101",
+    "url": "/expo-gh-pages/favicon.ico"
+  },
+  {
+    "revision": "39c2a65430b265587dc4a7d5d7cfc29f",
+    "url": "/expo-gh-pages/index.html"
+  },
+  {
+    "revision": "d6198ae63a4392951b167704d3d27a4e",
+    "url": "/expo-gh-pages/manifest.json"
+  },
+  {
+    "revision": "5a38be2e14ab56edb23778e69121729d",
+    "url": "/expo-gh-pages/register-service-worker.js"
+  }
+];
